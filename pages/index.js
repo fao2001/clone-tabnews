07bd31996335443328958900e4ca5a9ae7178d31
fao2001 <42865005+fao2001@users.noreply.github.com>
2024-04-29 00:40:23 +0000

    modifica a frase

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 function Home(){
 
-    return <h1>Como não tenho para quem enviar, fica para só como teste mesmo.. rss</h1>
+    return <h1>Como não tenho para quem enviar, fica para só como teste mesmo.. rss :-) </h1>
 }
 
 export default Home;
